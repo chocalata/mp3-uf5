e34287b09568224f727dcf83c422e102d0b64358
Catalin <cata627@gmail.com>
2018-12-20 01:39:43 +0100

    Acabado

--- excepcions/ActivitatExceptions/Wait.java
+++ excepcions/ActivitatExceptions/Wait.java
@@ -3,7 +3,7 @@ package excepcions.ActivitatExceptions;
 import java.util.Scanner;
 
 public class Wait {
-    //Espera a que el usuario pulse intro.
+    //Espera a que el usuario pulse intro
     public Wait(){
         System.out.println("\nPulse intro para continuar.");
         new Scanner(System.in).nextLine();
